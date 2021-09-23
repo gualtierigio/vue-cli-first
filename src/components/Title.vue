8962e2c69ddb223c926732584f30d940ba21762a
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-09-23 16:20:26 +0200

    hero creato

---
+++ src/components/Title.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>La mia prima app con Vue CLI!</h1>
+</template>
+<script>
+
+</script>
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
